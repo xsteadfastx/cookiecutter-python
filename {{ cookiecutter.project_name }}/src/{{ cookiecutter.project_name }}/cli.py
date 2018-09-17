444ea54a08117bf97ba4e606d82d374dcf04dd82
Marvin Steadfast <marvin@xsteadfastx.org>
2018-09-17 14:22:05 +0200

    install click and cli in default
    this should make it more easier for creating shell scripts.

---
+++ {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}/cli.py
@@ -0,0 +1,14 @@
+"""Console script for {{ cookiecutter.project_name }}"""
+import click
+
+
+@click.command()
+def main():
+    """{{ cookiecutter.project_short_description }}"""
+    click.echo(
+        (
+            "Replace this message by putting your code into "
+            "{{ cookiecutter.project_name }}.cli.main"
+        )
+    )
+    click.echo("See click documentation at http://click.pocoo.org/")
