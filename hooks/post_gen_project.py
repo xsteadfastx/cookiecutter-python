33406aa5bb118321928d9e969ea89798d69fd230
Marvin Steadfast <marvin@xsteadfastx.org>
2019-02-20 13:08:43 +0100

    moves dep installing to post get hook script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,7 @@ import sys
 
 RETURN_CODES = []
 
+# init git
 RETURN_CODES.append(subprocess.run(["git", "init"]).returncode)
 RETURN_CODES.append(
     subprocess.run(
@@ -14,6 +15,31 @@ RETURN_CODES.append(
     subprocess.run(
         ["git", "config", "--add", "user.email", "{{ cookiecutter.author_email }}"]
     ).returncode
+)
+
+# install deps through poetry
+DEPS = [
+    "click"
+]
+
+RETURN_CODES.append(
+    subprocess.run(["poetry", "add"] + DEPS).returncode
+)
+
+DEV_DEPS = [
+    "black==18.9b0",
+    "codecov",
+    "flake8",
+    "isort",
+    "mypy",
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "tox",
+]
+
+RETURN_CODES.append(
+    subprocess.run(["poetry", "add", "--dev"] + DEV_DEPS).returncode
 )
 
 if any(RETURN_CODES):
